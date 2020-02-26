3c6d04a8427b082efce7a033dffbae5fc40ad0a1
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-26 10:10:37 -0600

    chat works on react with server

--- src/ChatContainer/index.js
+++ src/ChatContainer/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import 'whatwg-fetch'
+import openSocket from 'socket.io-client'
+const socket = openSocket('http://localhost:8000')
 
 export default class ChatContainer extends Component {
 	constructor(){
 		super()
+
+		this.sendSocketIo = this.sendSocketIo.bind(this)
+	}
+
+	sendSocketIo() {
+
+
+
+		socket.emit('example_message', 'My chat works!')
+
+
 	}
 
 	render(){
 		return(
 			<React.Fragment>
-				ChatContainer
+				<button onClick={this.sendSocketIo}>Send Socket.io</button>
 			</React.Fragment>
 		)
 	}
