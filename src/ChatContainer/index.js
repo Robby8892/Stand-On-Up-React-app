2f435c427a08bb464947bd43bbd238711ce2d9ef
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-26 09:36:46 -0600

    created ChatContainer component

---
+++ src/ChatContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class ChatContainer extends Component {
+	constructor(){
+		super()
+	}
+
+	render(){
+		return(
+			<React.Fragment>
+				ChatContainer
+			</React.Fragment>
+		)
+	}
+}
