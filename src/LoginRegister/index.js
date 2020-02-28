cf960c6576048d91764fce783068a4508629e40a
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-28 16:34:10 -0600

    have a functional component for loginReg with a  form

--- src/LoginRegister/index.js
+++ src/LoginRegister/index.js
@@ -4,13 +4,61 @@ import { Button, Form, Segment } from 'semantic-ui-react'
 export default class LoginRegister extends Component {
 	constructor(pros){
 		super(pros)
+
+		this.state = {
+			user: {
+				username: '', 
+				password: '',
+				email: '',
+				firstName: '',
+				lastName: ''
+			}
+		}
+	}
+
+	handleChange = (e) => {
+		console.log('Here is handleChange');
 	}
 
 	render(){
 		return(
-			<React.Fragment>
-				LoginRegister
-			</React.Fragment>
+			<Segment inverted>
+				<Form inverted>
+					<Form.Group widths='equal'>
+						<Form.Input 
+						type='text' 
+						name='username' 
+						placeholder='Enter username'
+						value={this.state.username} 
+						/>
+						<Form.Input 
+						type='password' 
+						name='password' 
+						placeholder='Enter password'
+						value={this.state.password} 
+						/>
+						<Form.Input 
+						type='text' 
+						name='email'
+						placeholder='Enter email'
+						value={this.state.email}
+						/>
+						<Form.Input 
+						type='text'
+						name='firstName' 
+						placeholder='Enter first name'
+						value={this.state.firstName}
+						/>
+						<Form.Input 
+						type='text'
+						name='lastName' 
+						placeholder='Enter last name'
+						value={this.state.lastName}
+						/>
+					</Form.Group>
+					<Button type='submit'>Register</Button>
+				</Form>
+			</Segment>
 	)
 	}
 }
